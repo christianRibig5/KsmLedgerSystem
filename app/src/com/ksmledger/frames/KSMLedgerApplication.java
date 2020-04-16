94a2a2d7ac9736bfba00169a9b4ad212e1e82834
christianonyeukwu <christianonyeukwu@gmail.com>
2020-04-16 07:53:32 -0700

    updated the applications initializables

--- app/src/com/ksmledger/frames/KSMLedgerApplication.java
+++ app/src/com/ksmledger/frames/KSMLedgerApplication.java
@@ -1,4 +1,4 @@
-package sample;
+package com.ksmledger.frames;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,13 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class KSMLedgerApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLSplashScreen.fxml"));
+        Scene scene= new Scene(root);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
---
+++ app/src/com/ksmledger/utils/ConnectionUtil.java
@@ -0,0 +1,20 @@
+package com.ksmledger.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ConnectionUtil {
+    private static Connection conn = null;
+    public static Connection connectDB(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/ksmledgerdb?useTimezone=true&serverTimezone=UTC","root","");
+            return conn;
+        }catch (Exception ex){
+            Logger.getLogger(ConnectionUtil.class.getName()).log(Level.SEVERE, null,ex);
+            return  null;
+        }
+    }
+}
